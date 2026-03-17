fe16cd9c95b5e1a7ed7dfcd3f5ed453ddadd7180
Russell Shahenian <russ85226@gmail.com>
2026-03-16 20:16:43 -0700

    Update for php.

---
+++ php_lessons/ch1-lesson-4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ch1 PHP</title>
+	
+	</head>
+	<body>
+
+		<h1>Welcome to PHP!</h1>
+
+
+		<?php
+            $name = "Russell S";
+            echo "Hello ".$name;
+            echo "<br>";
+            echo "Hello $name";
+            echo "<br>";
+			echo "Hello World";
+            echo "<br>";
+			echo "This is some php, russell";
+            echo "<br>";
+		?>
+
+
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+		
+	</body>
+</html>
